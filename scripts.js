c3b16e3b101adc71eb60555bbbf7e17942ee18d4
shrvn12 <singhshravan1208@gmail.com>
2025-07-18 15:06:01 +0530

    updated data and visual enhancements

--- scripts.js
+++ scripts.js
@@ -1,15 +1,3 @@
-// window.addEventListener("load",()=>{
-//     document.querySelector(".load").style.display = "none"
-// })
-
-
-// let resume = document.querySelectorAll(".resumepage");
-// for(let elem of resume){
-//     elem.addEventListener("click",()=>{
-//         window.open("https://drive.google.com/file/d/1q2-ZcMYcWeQoXFp150dEw8LNWl83Ovwo/view?usp=sharing","_blank");
-//     })        
-// }
-
 function resume(){
     window.open("https://drive.google.com/file/d/1q2-ZcMYcWeQoXFp150dEw8LNWl83Ovwo/view?usp=sharing","_blank");
 }
@@ -66,3 +54,49 @@ if(window.innerWidth < 870){
 document.querySelector('.home-section>div>img').addEventListener('click',() => {
     window.location.href = "https://shrvn12.gthub.io#about"
 })
+
+const navbar = document.getElementById('nav');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 10) {
+    navbar.classList.add('scrolled');
+  } else {
+    navbar.classList.remove('scrolled');
+  }
+});
+
+
+GitHubCalendar(".calendar", "shrvn12");
+
+    GitHubCalendar(".calendar", "shrvn12", { responsive: true });
+
+    GitHubCalendar(".calendar", "shrvn12", {
+       proxy (username) {
+         return fetch(`https://your-proxy.com/github?user=${username}`)
+       }
+    }).then(r => r.text())
+
+
+	const buttons = [
+		{ element: document.querySelector(".filling-button"), timeout: 3600 },
+	];
+
+	buttons.forEach((button) => {
+		const { element, timeout } = button;
+
+		if (!element) return;
+
+		element.addEventListener("click", (event) => {
+            console.log('clicked');
+			event.preventDefault();
+
+			if (element.classList.contains("completed")) return;
+
+			element.classList.add("active");
+
+			setTimeout(() => {
+				element.classList.remove("active");
+				element.classList.add("completed");
+			}, timeout);
+		});
+	});
